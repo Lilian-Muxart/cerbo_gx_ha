0cead51b3bd8070b94f2978c2482d372cad34ced
MUXART <lilian.muxart@gmail.com>
2024-12-16 10:14:16 +0100

    test5

--- custom_components/cerbo_gx/const.py
+++ custom_components/cerbo_gx/const.py
@@ -3,6 +3,5 @@ DOMAIN = "cerbo_gx"
 # Paramètres de configuration utilisés pour l'intégration
 CONF_DEVICE_NAME = "device_name"
 CONF_CERBO_ID = "cerbo_id"
-CONF_ROOM = "room"
 CONF_USERNAME = "username"
 CONF_PASSWORD = "password"
