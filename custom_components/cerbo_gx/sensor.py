d80d0b1c2f284236cac4199854988dfee2a566d3
MUXART <lilian.muxart@gmail.com>
2024-12-16 08:47:25 +0100

    test5

--- custom_components/cerbo_gx/sensor.py
+++ custom_components/cerbo_gx/sensor.py
@@ -1,7 +1,6 @@
 import logging
 import json
 from homeassistant.components.sensor import SensorEntity
-from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.typing import HomeAssistantType
 from homeassistant.components.sensor import SensorDeviceClass
 from . import DOMAIN
@@ -12,14 +11,17 @@ async def async_setup_entry(hass: HomeAssistantType, entry, async_add_entities) 
     """Configurer les capteurs pour une entrée donnée."""
     device_name = entry.data["device_name"]
     id_site = entry.data["cerbo_id"]
+    area_id = entry.data.get("area_id")  # Récupérer la zone associée
 
-    _LOGGER.info("Initialisation des capteurs pour le dispositif %s avec l'ID de site %s", device_name, id_site)
+    _LOGGER.info(
+        "Initialisation des capteurs pour le dispositif %s avec l'ID de site %s", device_name, id_site
+    )
 
     # Liste des capteurs à ajouter
     sensors = [
-        CerboBatterySensor(device_name, id_site),
-        CerboVoltageSensor(device_name, id_site),
-        CerboTemperatureSensor(device_name, id_site),
+        CerboBatterySensor(device_name, id_site, area_id),
+        CerboVoltageSensor(device_name, id_site, area_id),
+        CerboTemperatureSensor(device_name, id_site, area_id),
     ]
 
     # Ajouter les capteurs
@@ -28,209 +30,67 @@ async def async_setup_entry(hass: HomeAssistantType, entry, async_add_entities) 
     _LOGGER.info("Capteurs ajoutés pour %s", device_name)
 
 
-class CerboBatterySensor(SensorEntity):
+class CerboBaseSensor(SensorEntity):
+    """Classe de base pour les capteurs du Cerbo GX."""
+
+    def __init__(self, device_name: str, id_site: str, area_id: str):
+        """Initialiser le capteur."""
+        self._device_name = device_name
+        self._id_site = id_site
+        self._area_id = area_id
+        self._state = None
+        self._attr_device_info = {
+            "identifiers": {(DOMAIN, id_site)},
+            "name": device_name,
+            "manufacturer": "Victron Energy",
+            "model": "Cerbo GX",
+            "suggested_area": area_id,  # Associer l'appareil à une pièce
+        }
+
+
+class CerboBatterySensor(CerboBaseSensor):
     """Capteur pour la batterie du Cerbo GX."""
 
-    def __init__(self, device_name: str, id_site: str):
-        """Initialiser le capteur."""
-        self._name = f"{device_name} Battery Percent"
-        self._unique_id = f"{device_name}_battery_percent"
-        self._state = None
-        self._device_class = SensorDeviceClass.BATTERY
-        self._unit_of_measurement = "%"
+    def __init__(self, device_name: str, id_site: str, area_id: str):
+        super().__init__(device_name, id_site, area_id)
+        self._attr_name = f"{device_name} Battery Percent"
+        self._attr_unique_id = f"{id_site}_battery_percent"
+        self._attr_device_class = SensorDeviceClass.BATTERY
+        self._attr_native_unit_of_measurement = "%"
         self._state_topic = f"N/{id_site}/system/0/Batteries"
-        _LOGGER.debug("Capteur de batterie initialisé pour %s avec topic %s", device_name, self._state_topic)
-
-    @property
-    def name(self):
-        """Retourner le nom du capteur."""
-        return self._name
-
-    @property
-    def unique_id(self):
-        """Retourner l'ID unique du capteur."""
-        return self._unique_id
-
-    @property
-    def device_class(self):
-        """Retourner la classe du dispositif."""
-        return self._device_class
 
     @property
     def state(self):
-        """Retourner l'état actuel du capteur."""
         return self._state
 
-    @property
-    def unit_of_measurement(self):
-        """Retourner l'unité de mesure."""
-        return self._unit_of_measurement
 
-    async def async_update(self):
-        """Mettre à jour l'état du capteur avec les données MQTT."""
-        _LOGGER.debug("Demande de mise à jour du capteur de batterie %s", self._name)
-        pass
-
-    def on_message(self, client, userdata, msg):
-        """Gérer la réception de messages MQTT pour la batterie."""
-        _LOGGER.debug("Message reçu sur le topic : %s", msg.topic)
-
-        # Vérifie si le message provient du bon topic
-        if msg.topic == self._state_topic:
-            try:
-                # Décoder le message JSON
-                payload = json.loads(msg.payload.decode())
-                _LOGGER.debug("Payload reçu : %s", payload)
-
-                # Vérifier que 'value' est une liste et qu'elle contient un élément
-                if isinstance(payload, dict) and 'value' in payload:
-                    battery_data = payload.get("value", [])[0]  # Prend le premier élément de la liste
-                    if battery_data and "soc" in battery_data:
-                        self._state = battery_data["soc"]  # Récupère le pourcentage de charge de la batterie
-                        _LOGGER.info("État de la batterie mis à jour : %s%%", self._state)
-                    else:
-                        _LOGGER.warning("Clé 'soc' manquante ou mauvaise structure des données de batterie.")
-                else:
-                    _LOGGER.warning("Le message ne contient pas de clé 'value' ou structure invalide.")
-
-                # Mettre à jour l'état du capteur dans Home Assistant
-                self.schedule_update_ha_state()
-
-            except json.JSONDecodeError:
-                _LOGGER.error("Erreur de décodage JSON pour le message de batterie : %s", msg.payload)
-            except KeyError as e:
-                _LOGGER.error("Clé manquante dans les données de batterie : %s", e)
-
-
-class CerboVoltageSensor(SensorEntity):
+class CerboVoltageSensor(CerboBaseSensor):
     """Capteur pour la tension du Cerbo GX."""
 
-    def __init__(self, device_name: str, id_site: str):
-        """Initialiser le capteur."""
-        self._name = f"{device_name} Voltage"
-        self._unique_id = f"{device_name}_voltage"
-        self._state = None
-        self._device_class = SensorDeviceClass.VOLTAGE
-        self._unit_of_measurement = "V"
+    def __init__(self, device_name: str, id_site: str, area_id: str):
+        super().__init__(device_name, id_site, area_id)
+        self._attr_name = f"{device_name} Voltage"
+        self._attr_unique_id = f"{id_site}_voltage"
+        self._attr_device_class = SensorDeviceClass.VOLTAGE
+        self._attr_native_unit_of_measurement = "V"
         self._state_topic = f"N/{id_site}/system/0/Voltage"
-        _LOGGER.debug("Capteur de tension initialisé pour %s avec topic %s", device_name, self._state_topic)
-
-    @property
-    def name(self):
-        """Retourner le nom du capteur."""
-        return self._name
-
-    @property
-    def unique_id(self):
-        """Retourner l'ID unique du capteur."""
-        return self._unique_id
-
-    @property
-    def device_class(self):
-        """Retourner la classe du dispositif."""
-        return self._device_class
 
     @property
     def state(self):
-        """Retourner l'état actuel du capteur."""
         return self._state
 
-    @property
-    def unit_of_measurement(self):
-        """Retourner l'unité de mesure."""
-        return self._unit_of_measurement
 
-    async def async_update(self):
-        """Mettre à jour l'état du capteur avec les données MQTT."""
-        _LOGGER.debug("Demande de mise à jour du capteur de tension %s", self._name)
-        pass
-
-    def on_message(self, client, userdata, msg):
-        """Gérer la réception de messages MQTT pour la tension."""
-        _LOGGER.debug("Message reçu sur le topic %s", msg.topic)
-
-        if msg.topic == self._state_topic:
-            try:
-                payload = json.loads(msg.payload.decode())
-                _LOGGER.debug("Payload reçu pour la tension : %s", payload)
-
-                # Extraire la valeur 'voltage' (tension)
-                self._state = payload.get("voltage", None)
-                if self._state is not None:
-                    _LOGGER.info("Tension mise à jour : %s V", self._state)
-                else:
-                    _LOGGER.warning("Clé 'voltage' manquante dans le payload de tension.")
-
-                # Mettre à jour l'état dans Home Assistant
-                self.schedule_update_ha_state()
-            except json.JSONDecodeError:
-                _LOGGER.error("Erreur de décodage JSON pour le message de tension : %s", msg.payload)
-            except KeyError as e:
-                _LOGGER.error("Clé manquante dans les données de tension : %s", e)
-
-
-class CerboTemperatureSensor(SensorEntity):
+class CerboTemperatureSensor(CerboBaseSensor):
     """Capteur pour la température du Cerbo GX."""
 
-    def __init__(self, device_name: str, id_site: str):
-        """Initialiser le capteur."""
-        self._name = f"{device_name} Temperature"
-        self._unique_id = f"{device_name}_temperature"
-        self._state = None
-        self._device_class = SensorDeviceClass.TEMPERATURE
-        self._unit_of_measurement = "°C"
+    def __init__(self, device_name: str, id_site: str, area_id: str):
+        super().__init__(device_name, id_site, area_id)
+        self._attr_name = f"{device_name} Temperature"
+        self._attr_unique_id = f"{id_site}_temperature"
+        self._attr_device_class = SensorDeviceClass.TEMPERATURE
+        self._attr_native_unit_of_measurement = "°C"
         self._state_topic = f"N/{id_site}/system/0/Temperature"
-        _LOGGER.debug("Capteur de température initialisé pour %s avec topic %s", device_name, self._state_topic)
-
-    @property
-    def name(self):
-        """Retourner le nom du capteur."""
-        return self._name
-
-    @property
-    def unique_id(self):
-        """Retourner l'ID unique du capteur."""
-        return self._unique_id
-
-    @property
-    def device_class(self):
-        """Retourner la classe du dispositif."""
-        return self._device_class
 
     @property
     def state(self):
-        """Retourner l'état actuel du capteur."""
         return self._state
-
-    @property
-    def unit_of_measurement(self):
-        """Retourner l'unité de mesure."""
-        return self._unit_of_measurement
-
-    async def async_update(self):
-        """Mettre à jour l'état du capteur avec les données MQTT."""
-        _LOGGER.debug("Demande de mise à jour du capteur de température %s", self._name)
-        pass
-
-    def on_message(self, client, userdata, msg):
-        """Gérer la réception de messages MQTT pour la température."""
-        _LOGGER.debug("Message reçu sur le topic %s", msg.topic)
-
-        if msg.topic == self._state_topic:
-            try:
-                payload = json.loads(msg.payload.decode())
-                _LOGGER.debug("Payload reçu pour la température : %s", payload)
-
-                # Extraire la valeur 'temperature' (température)
-                self._state = payload.get("temperature", None)
-                if self._state is not None:
-                    _LOGGER.info("Température mise à jour : %s °C", self._state)
-                else:
-                    _LOGGER.warning("Clé 'temperature' manquante dans le payload de température.")
-
-                # Mettre à jour l'état dans Home Assistant
-                self.schedule_update_ha_state()
-            except json.JSONDecodeError:
-                _LOGGER.error("Erreur de décodage JSON pour le message de température : %s", msg.payload)
-            except KeyError as e:
-                _LOGGER.error("Clé manquante dans les données de température : %s", e)
